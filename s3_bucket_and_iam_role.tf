4b5ee8d58a823270a545d6684d9eb35da16a5e71
Michael Zeevi <michael.52@msn.com>
2021-05-22 11:51:25 +0300

    improved terraform for prerequisites

--- s3_bucket_and_iam_role.tf
+++ s3_bucket_and_iam_role.tf
@@ -3,7 +3,8 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "mz-demo-bucket"
+  // CHANGEME!
+  bucket = "mz-demo-bucket-tf"
 }
 
 resource "aws_s3_bucket_public_access_block" "demo_bucket" {
@@ -15,52 +16,50 @@ resource "aws_s3_bucket_public_access_block" "demo_bucket" {
 }
 
 resource "aws_iam_policy" "ls_bucket_and_all_object_actions" {
-  name   = "${aws_iam_role.demo_eks_oidc_s3_bucket_accesser.name}"
+  name   = "demo-eks-oidc-s3-bucket-accesser-tf"
   path   = "/"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "LsBucketAndAllObjectActions",
-      "Effect": "Allow",
-      "Action": [
-        "s3:ListBucket",
-        "s3:*Object"
-      ],
-      "Resource": [
-        "arn:aws:s3:::mz-demo-bucket",
-        "arn:aws:s3:::mz-demo-bucket/*"
-      ]
-    }
-  ]
-}
-EOF
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action   = [
+          "s3:ListBucket",
+          "s3:*Object"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.demo_bucket.id}",
+          "arn:aws:s3:::${aws_s3_bucket.demo_bucket.id}/*"
+        ]
+      },
+    ]
+  })
 }
 
 resource "aws_iam_role" "demo_eks_oidc_s3_bucket_accesser" {
-  name               = "demo-eks-oidc-s3-bucket-accesser"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "arn:aws:iam::291615460784:oidc-provider/oidc.eks.eu-west-2.amazonaws.com/id/71E28602A1EF7B2894FBD143868BDC38"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "oidc.eks.eu-west-2.amazonaws.com/id/71E28602A1EF7B2894FBD143868BDC38:sub": "system:serviceaccount:default:demo-s3er"
+  name               = aws_iam_policy.ls_bucket_and_all_object_actions.name
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRoleWithWebIdentity"
+        Effect    = "Allow"
+        Principal = {
+          // CHANGEME! (account number, region, id)
+          "Federated" = "arn:aws:iam::291615460784:oidc-provider/oidc.eks.eu-west-2.amazonaws.com/id/71E28602A1EF7B2894FBD143868BDC38"
+        }
+        Condition = {
+          StringEquals = {
+            // CHANGEME! (account number, region, id)
+            "oidc.eks.eu-west-2.amazonaws.com/id/71E28602A1EF7B2894FBD143868BDC38:sub" = "system:serviceaccount:default:demo-s3er"
+          }
         }
       }
-    }
-  ]
+    ]
+  })
 }
-EOF
-  max_session_duration = 3600
-  tags {
-    owner = "Michael Zeevi"
-  }
+
+resource "aws_iam_role_policy_attachment" "demo_eks_oidc_s3_bucket_accesser" {
+  policy_arn = aws_iam_policy.demo_eks_oidc_s3_bucket_accesser.arn
+  role       = aws_iam_role.demo_eks_oidc_s3_bucket_accesser.name
 }
